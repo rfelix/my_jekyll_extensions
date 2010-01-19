55a307ba4066bd4ac789c9b9220912e252a6820f
Raoul Felix <rf@rfelix.com>
2010-01-19 17:20:03 +0000

    Updated readme and added comment to jekyll_ext entry point file

--- jekyll_ext.rb
+++ jekyll_ext.rb
@@ -1,3 +1,6 @@
+# This is the jekyl_ext entry point. This file must load all the extensions
+# that will be used when generating your blog
+
 Dir[File.join(File.dirname(__FILE__),"*", "*.rb")].each do |f| 
   unless f =~ /jekyll\_ext\.rb$/
     puts "Loading Extension: #{File.basename(f)}"
