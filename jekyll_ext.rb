74b79e33a9f290ffc25c13ac42885713f04f5a0b
Raoul Felix <rf@rfelix.com>
2010-01-19 02:01:14 +0000

    Separated each extension into its own directory and added sample data so others can see how to use the extensions.

--- jekyll_ext.rb
+++ jekyll_ext.rb
@@ -1,4 +1,4 @@
-Dir[File.join(File.dirname(__FILE__), "*.rb")].each do |f| 
+Dir[File.join(File.dirname(__FILE__),"*", "*.rb")].each do |f| 
   unless f =~ /jekyll\_ext\.rb$/
     puts "Loading Extension: #{File.basename(f)}"
     load f
