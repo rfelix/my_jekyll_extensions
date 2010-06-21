f7cc559a262718dc7e2a186a2f486f7622293701
Raoul Felix <rf@rfelix.com>
2010-06-21 12:25:59 +0100

    jekyll_ext.rb is not needed anymore with gem version of jekyll_ext

--- jekyll_ext.rb
+++
@@ -1,9 +0,0 @@
-# This is the jekyl_ext entry point. This file must load all the extensions
-# that will be used when generating your blog
-
-Dir[File.join(File.dirname(__FILE__),"*", "*.rb")].each do |f| 
-  unless f =~ /jekyll\_ext\.rb$/
-    puts "Loading Extension: #{File.basename(f)}"
-    load f
-  end
-end
